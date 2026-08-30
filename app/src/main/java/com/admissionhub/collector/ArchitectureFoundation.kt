b8b62555d2755b9511b6121d1adcfb4425572155
gigagene1234-sys <gigagene1234@gmail.com>
2026-08-31 01:51:24 +0900

    Finalize v0.7.0 committed-source architecture marker

---
+++ app/src/main/java/com/admissionhub/collector/ArchitectureFoundation.kt
@@ -0,0 +1,8 @@
+package com.admissionhub.collector
+
+object ArchitectureFoundation {
+    const val SCHEMA_VERSION = 4
+    const val OBSERVATION_FIRST = true
+    const val CONTENT_AWARE_DEDUPE = true
+    const val CLEAN_COMMITTED_SOURCE_BUILD = true
+}
