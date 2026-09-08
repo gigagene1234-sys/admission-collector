9a346da8fb265acd70acb0c74ab44eaf4c261d29
gigagene1234-sys <gigagene1234@gmail.com>
2026-09-08 20:25:44 +0900

    test: enforce Jinhak lower-grade login route fence

--- app/src/test/java/com/admissionhub/collector/jinhak/JinhakGradeRouteFenceTest.kt
+++ app/src/test/java/com/admissionhub/collector/jinhak/JinhakGradeRouteFenceTest.kt
@@ -12,9 +12,33 @@ class JinhakGradeRouteFenceTest {
     }
 
     @Test
-    fun high3AndLoginRemainAllowed() {
+    fun encodedLowerGradeLoginDestinationsAreBlockedBeforeNavigation() {
+        assertTrue(
+            JinhakGradeRouteFence.isBlockedLowerGrade(
+                "https://www.jinhak.com/jh/member/login?returnUrl=%2Fjh%2Fhigh1%2Fhome"
+            )
+        )
+        assertTrue(
+            JinhakGradeRouteFence.isBlockedLowerGrade(
+                "https://www.jinhak.com/jh/member/login?next=%2Fjh%2Fhigh2%2Fearly%2Fsearch"
+            )
+        )
+        assertTrue(
+            JinhakGradeRouteFence.isBlockedLowerGrade(
+                "https://www.jinhak.com/jh/member/login#redirect=/jh/high12/main"
+            )
+        )
+    }
+
+    @Test
+    fun high3AndSharedLoginWithoutLowerGradeContextRemainAllowed() {
         assertFalse(JinhakGradeRouteFence.isBlockedLowerGrade("https://www.jinhak.com/jh/high3/early/four-year-university/library"))
         assertFalse(JinhakGradeRouteFence.isBlockedLowerGrade("https://www.jinhak.com/jh/member/login"))
+        assertFalse(
+            JinhakGradeRouteFence.isBlockedLowerGrade(
+                "https://www.jinhak.com/jh/member/login?returnUrl=%2Fjh%2Fhigh3%2Fearly%2Ffour-year-university%2Flibrary"
+            )
+        )
         assertTrue(JinhakGradeRouteFence.isHigh3("https://www.jinhak.com/jh/high3/early/four-year-university/library"))
     }
 
