5194d2b2a9148466d1cb57e3bbdcea9df109fc6d
gigagene1234-sys <gigagene1234@gmail.com>
2026-09-09 09:12:50 +0900

    v0.17.0: update auth event tests for passive site login

--- app/src/test/java/com/admissionhub/collector/jinhak/JinhakAuthEventStateTest.kt
+++ app/src/test/java/com/admissionhub/collector/jinhak/JinhakAuthEventStateTest.kt
@@ -2,7 +2,6 @@ package com.admissionhub.collector.jinhak
 
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class JinhakAuthEventStateTest {
@@ -21,31 +20,29 @@ class JinhakAuthEventStateTest {
     }
 
     @Test
-    fun genericProductLoginIsOneShotRewriteOnly() {
+    fun genericProductLoginIsPassiveSiteOwnedWait() {
         val url = "https://www.jinhak.com/jh/member/login"
-        assertEquals(JinhakAuthEventState.Event.GENERIC_PRODUCT_LOGIN, JinhakAuthEventState.classify(url))
-        assertEquals(JinhakAuthEventState.Action.OPEN_CANONICAL_HIGH3_AUTH_ONCE, JinhakAuthEventState.action(url))
+        assertEquals(JinhakAuthEventState.Event.MEMBER_LOGIN, JinhakAuthEventState.classify(url))
+        assertEquals(JinhakAuthEventState.Action.WAIT_FOR_SERVER_RETURN, JinhakAuthEventState.action(url))
     }
 
     @Test
-    fun canonicalMemberLoginWaitsForServerReturn() {
-        val high3 = JinhakGradeRouteFence.protectedHigh3Core()
-        val login = JinhakHigh3AuthRoute.canonicalLoginUrl(high3)
-        assertTrue(JinhakHigh3AuthRoute.isMemberLoginSurface(login))
+    fun memberLoginWaitsForServerReturnWithoutCollectorRewrite() {
+        val login = "https://member.jinhak.com/MemberV3/MemberJoin/MemberLogIn.aspx"
         assertEquals(JinhakAuthEventState.Event.MEMBER_LOGIN, JinhakAuthEventState.classify(login))
         assertEquals(JinhakAuthEventState.Action.WAIT_FOR_SERVER_RETURN, JinhakAuthEventState.action(login))
         assertFalse(JinhakGradeRouteFence.isBlockedLowerGrade(login))
     }
 
     @Test
-    fun high3IsVerifiedOnlyAfterReturn() {
+    fun high3NavigationEventIsHandedToCollectorWithoutLoginRewrite() {
         val high3 = JinhakGradeRouteFence.protectedHigh3Core()
         assertEquals(JinhakAuthEventState.Event.HIGH3, JinhakAuthEventState.classify(high3))
         assertEquals(JinhakAuthEventState.Action.VERIFY_HIGH3_SESSION, JinhakAuthEventState.action(high3))
     }
 
     @Test
-    fun unknownSameProviderPageIsObservedNotStopped() {
+    fun unknownHigh3PageIsStillHigh3AndNotStopped() {
         val url = "https://www.jinhak.com/jh/high3/new-future-page"
         assertEquals(JinhakAuthEventState.Event.HIGH3, JinhakAuthEventState.classify(url))
         assertEquals(JinhakAuthEventState.Action.VERIFY_HIGH3_SESSION, JinhakAuthEventState.action(url))
